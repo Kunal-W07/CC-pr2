0dd46d607b51898bbf469144033678539fec0eec
kunal_07 <165695901+Kunal-W07@users.noreply.github.com>
2025-04-20 20:24:27 +0530

    Create pr2.py

---
+++ pr2.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+# Taking input from user
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    print(f"Factorial of {num} is {factorial(num)}")
